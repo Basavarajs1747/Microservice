ecccfce5c6bd2f6a93396748961113b516b81a03
Basavarajs1747 <bsiddapur1747@gmail.com>
2025-12-03 23:08:28 +0530

    Add files via upload

---
+++ notificationservice/src/main/java/com/notification/consumer/EmailConsumer.java
@@ -0,0 +1,28 @@
+package com.notification.consumer;
+
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import com.notification.dto.EmailRequest;
+
+@Service
+public class EmailConsumer {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @KafkaListener(topics = "send_email", groupId = "notification-group")
+    public void consume(EmailRequest request) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(request.getTo());
+        message.setSubject(request.getSubject());
+        message.setText(request.getBody());
+
+        mailSender.send(message);
+    }
+}
